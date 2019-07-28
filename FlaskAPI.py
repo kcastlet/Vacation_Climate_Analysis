ed0e99331be57a75fdb85eabca21995c0f06c701
Baroness Blixen <47258590+kcastlet@users.noreply.github.com>
2019-07-27 23:00:23 -0500

    code changes

--- FlaskAPI.py
+++ FlaskAPI.py
@@ -5,59 +5,46 @@
 # /api/v1.0/<start>/<end> where start and end are dates in YYYY-MM-DD format
 
 from flask import Flask, jsonify
-
+import pandas as pd
+import numpy as np
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func, inspect
 
-import numpy as np
-
 engine = create_engine("sqlite:///Resources/hawaii.sqlite")
 
-#Reflect database into new model
+#Reflect database
 Base = automap_base()
-#Reflect tables
 Base.prepare(engine, reflect=True)
 
-#Save references to each table
+# Connect to database 
+session = Session(engine)
+conn = engine.connect()
+
+#Save classes to tables
 measurement = Base.classes.measurement
 station = Base.classes.station
 
-# Create session to connect to database 
-session = Session(engine)
 
 def calc_temps(start_date, end_date):
-    """TMIN, TAVG, and TMAX for a list of dates.
-    
-    Args:
-        start_date (string): A date string in the format %Y-%m-%d
-        end_date (string): A date string in the format %Y-%m-%d
-        
-    Returns:
-        TMIN, TAVE, and TMAX
-    """
     
     return session.query(func.min(measurement.tobs), func.avg(measurement.tobs), func.max(measurement.tobs)).\
         filter(measurement.date >= start_date).filter(measurement.date <= end_date).all()
 
-# Flask
+#Open Flask module
 app = Flask(__name__)
 
-
-# Create routes
+#Match URLs to view functions in Flask
 @app.route("/")
-def greeting():
-    return """
-    Welcome to the API for Hawaii's climate.
-    Available endpoints: <br>
-    /api/v1.0/precipitation <br>
-    /api/v1.0/stations <br>
-    /api/v1.0/tobs <br>
-    /api/v1.0/&lt;start&gt; where start is a date in YYYY-MM-DD format <br> 
-    /api/v1.0/&lt;start&gt;/&lt;end&gt; where start and end are dates in YYYY-MM-DD format
-    """
-
+def home():
+    return 'Welcome to the API for the climate in Hawaii.'
+    'These links can be accessed:<br/>'.\
+		   '<a href="/api/v1.0/precipitation">/api/v1.0/precipitation</a><br/>'.\
+		   '<a href="/api/v1.0/stations">/api/v1.0/stations</a><br/>'.\
+		   '<a href="/api/v1.0/tobs">/api/v1.0/tobs</a><br/>'.\
+		   '<a href="/api/v1.0/&lt;start&gt;">/api/v1.0/&lt;start&gt;</a><br/>'.\
+		   '<a href="/api/v1.0/&lt;start&gt;/&lt;end&gt;">/api/v1.0/&lt;start&gt;/&lt;end&gt;</a>'
 
 @app.route("/api/v1.0/precipitation")
 def prcp():
@@ -74,13 +61,14 @@ def prcp():
     return jsonify(dict)
 
 
+
 @app.route("/api/v1.0/stations")
 def stations():
     results = session.query(station.station).all()
-
     results_list = list(np.ravel(results))
 
     return jsonify(results_list)
+
 
 
 @app.route("/api/v1.0/tobs")
@@ -88,7 +76,6 @@ def tobs():
     results = session.query(measurement.tobs).\
         filter(measurement.date >= "2016-08-23").\
         filter(measurement.date <= "2017-08-23").all()
-
     results_list = list(np.ravel(results))
 
     return jsonify(results_list)
@@ -100,13 +87,16 @@ def start_date(start):
     end_date = session.query(func.max(measurement.date)).all()[0][0]
     temps = calc_temps(start, end_date)
     temps_list = list(np.ravel(temps))
+
     return jsonify(temps_list)
+
 
 
 @app.route("/api/v1.0/<start>/<end>")
 def start_end_date(start, end):
     temps = calc_temps(start, end)
     temps_list = list(np.ravel(temps))
+
     return jsonify(temps_list)
 
 if __name__ == '__main__':
